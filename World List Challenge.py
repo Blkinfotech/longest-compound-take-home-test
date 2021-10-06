c4cd5ecbe18dc93595e9aee53bc5e26291334d98
Blkinfotech <74229137+Blkinfotech@users.noreply.github.com>
2021-10-06 11:53:40 -0700

    Add files via upload

---
+++ World List Challenge.py
@@ -0,0 +1,12 @@
+word_list = open('word_list.txt', 'r').read()
+    
+def find_longest_word(words_list.txt) :
+    longest_word = ''
+    for wdd in words:''
+    if len(wdd) > len(longest_word):
+         longest_word =wdd
+print ('The Longest word is' + longest_word)
+
+
+
+find_longest_word
